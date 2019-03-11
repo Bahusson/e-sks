fd908b97d1b5c803eee0f69524e25e80c6fc67c9
UncleSpook <unclespook@pc19.home>
2019-03-11 17:00:38 +0100

    Rozpisywanie i łączenie aplikacji
    Zdefiniowałem podstawowe aplikacje i wstępnie je podłączyłem.

--- esks/esks/settings.py
+++ esks/esks/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rekruter.apps.RekruterConfig',
+    'strona.apps.StronaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'esks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
--- esks/esks/urls.py
+++ esks/esks/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+import strona.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('kotelnica/', admin.site.urls), #zmyłka dla botów próbujących się dostać do strony administracji
+    path('', strona.views.home, name='home'),
+    path('logowanie/', strona.views.login, name='login'), #strona logowania - przenosi nas do wybranego podsystemu
+#    path('strona/', include('strona.urls')), #strona ogólna do komunikacji ze studentami.
+    path('rejestracja/', include('rekruter.urls')), #strona rejestracji do systemu akademików (dla studentów)
+    path('akademik/', include('akademik.urls')), #strona administracyjna dla rady studentów i administracji akademika
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #pliki statyczne ładowane tylko manualnie z poziomu serwera.
