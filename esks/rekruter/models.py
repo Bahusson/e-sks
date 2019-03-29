58ee3d9e2e214ea32f2366e8d57e77410f17d2e1
Master-Of-Disaster <Jakub.Kozdrowicz@pw.edu.pl>
2019-03-29 17:12:53 +0100

    Dopisane pola Sita rekrutera do translatora
    Do zrobienia jeszcze Zapis, Javascript (patrz instrukcja obsługi esks), i podstawowe templaty htmla.

--- esks/rekruter/models.py
+++ esks/rekruter/models.py
@@ -1,22 +1,24 @@
 from django.db import models
 
-class Sito(models.Model):
-    ### Część wspólna także dla klasy "Zapis", więc się upewnij, że dobrze wpisałeś ID języka.
-    # Zaczynasz od 0, gdzie 0 to Polski, a 1 to Angielski.
-    # Na sicie zawsze można podejrzeć który język jest który i TEN obowiązuje!
-    formlang_id = models.IntField(max_length=1) #Tu wpisz id jezyka po ktorym beda one uszeregowane w programie i wywolywane.
-    formlang_name = models.CharField(max_length=20) #To sie wyswietla w polu wybory dostepnych jezykow.
-    ### Koniec części wspólnej z klasą zapisu.
-    obywatelstwo = models.CharField(max_length=100)
-    obywatelstwo = models.CharField(max_length=100)
+class Sito(models.Model): #Sito pytań do akcji kwaterunkowej
+    intro = models.CharField(max_length=500)
+    oswiadczenie = models.TextField()
+    obywatelstwo = models.CharField(max_length=150)
+    student = models.CharField(max_length=150)
+    doktorant = models.CharField(max_length=150)
+    zamiar = models.CharField(max_length=150)
+    zadnezpow = models.CharField(max_length=150)
+    pelnywym = models.CharField(max_length=150)
+    erasmus = models.CharField(max_length=150)
+    bilateral = models.CharField(max_length=150)
+    buttondalej = models.CharField(max_length=100)
 
+    #Ta funkcja pokazuje tytuł postu na stronie admina. Zawsze używaj ___str___ żeby wrzucić coś w górę do adminów.
+    def __str__(self):
+        return self.title
 
 class Zapis(models.Model):
-    ### Część wspólna dla klasy "Sito". Po tym klasy się odnajdują.
-    # Szczegóły patrz wyżej w klasie "Sito".
-    formlang_id = models.IntField(max_length=1) #ID języka takie samo jak w Sicie!!
-    formlang_name = models.CharField(max_length=20) #Nazwa języka jak w Sicie. W programie jest tylko do orientacji, żeby w administracji nie przełączać się między klasami.
-    ### Koniec części wspólnej z klasą sita.
+
     imie = models.CharField(max_length=50) #Imie w danym jezyku (np. "name" po angielsku)
     imie_instrukcja = models.TextField() # Instrukcja dot wprowadzania imienia, nazwiska i znakow.
     nazwisko = models.CharField(max_length=50) #Nazwisko w danym jezyku (np. "surname" po angielsku)
---
+++ esks/rekruter/translation.py
@@ -0,0 +1,12 @@
+from modeltranslation.translator import translator, TranslationOptions
+from rekruter.models import Sito, Zapis
+
+class SitoTranslate(TranslationOptions):
+    fields = ('infro', 'oswiadczenie', 'obywatelstwo', 'student', 'doktorant', 'zamiar', 'zadnezpow', 'pelnywym', 'erasmus', 'bilateral', 'buttondalej')
+
+translator.register(Sito, SitoTranslate)
+
+class ZapisTranslate(TranslationOptions):
+    fields = ('?', '?')
+
+translator.register(Zapis, ZapisTranslate)
