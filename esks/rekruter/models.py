e76f3038286eff3e8c4a4b20d9ab9525825065e8
Jakub <politechnika>
2019-06-05 13:20:57 +0200

    Migracja do Postgresql przed wejściem demo do sieci
    Demo pozytywnie wypadło na testach w nowej bazie.

--- esks/rekruter/models.py
+++ esks/rekruter/models.py
@@ -13,15 +13,3 @@ class Sito(models.Model): #Sito pytań do akcji kwaterunkowej
     pelnywym = models.CharField(max_length=150)
     erasmus = models.CharField(max_length=150)
     buttondalej = models.CharField(max_length=100)
-
-#class Zapis(models.Model):
-
-#    imie = models.CharField(max_length=50) #Imie w danym jezyku (np. "name" po angielsku)
-#    imie_instrukcja = models.TextField() # Instrukcja dot wprowadzania imienia, nazwiska i znakow.
-#    nazwisko = models.CharField(max_length=50) #Nazwisko w danym jezyku (np. "surname" po angielsku)
-
-#Kontynuuj tak dalej ze wszystkimi elementami na stronie. ^^
-
-    #Ta funkcja pokazuje tytuł postu na stronie admina. Zawsze używaj ___str___ żeby wrzucić coś w górę do adminów.
-#    def __str__(self):
-#        return self.title
