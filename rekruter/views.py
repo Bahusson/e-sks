efcd4ecd1edadb0c6b9cf82952493e5ff1191e30
Jakub <politechnika>
2019-06-30 19:14:40 +0200

    Poprawiony login i rejestracja
    Struktura i wygląd. Wracam na swoją gałąź...

--- rekruter/views.py
+++ rekruter/views.py
@@ -4,10 +4,18 @@ from .models import Sito
 from strona.models import Pageitem as P
 from esks.settings import LANGUAGES as L
 from esks.special.classes import PageLoad
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
-def register(request):
-    pl = PageLoad(P,L)
+def initial(request):
+    pl = PageLoad(P, L)
     locations = list(Sito.objects.all())
     sitos = locations[0]
-    return render(request, 'rekruter/register.html', {'sitos': sitos, 'items': pl.items, 'langs': pl.langs})
+    return render(request, 'registration/initial.html', {'sitos': sitos, 'items': pl.items, 'langs': pl.langs})
+
+
+def register(request):
+    form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
