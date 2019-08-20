61116071070c8a6fcb0daba3d1d0b3aea6ac3787
Jakub <40240192+Bahusson@users.noreply.github.com>
2019-08-20 13:04:25 +0200

    Ustrukturyzowano Akademik
    Podłączony panel tłumacza, dodany wspólny śródpanel pośredni, zmodyfikowana klasa podstawowa i dostosowane tłumaczenie.

--- akademik/views.py
+++ akademik/views.py
@@ -3,7 +3,7 @@ from strona.models import Pageitem as P
 from strona.models import PageSkin as S
 from esks.settings import LANGUAGES as L
 from esks.special.classes import PageLoad
-from esks.special.decorators import council_only, hotel_staff_only
+from esks.special.decorators import council_only, hotel_staff_only, translators_only
 # from django.contrib.admin.views.decorators import staff_member_required
 from rekruter.models import User, QuarterClass
 
@@ -14,7 +14,7 @@ def staffpanel_c(request):
     # zdefiniuj dodatkowe konteksty tutaj.
     pl = PageLoad(P, L)
     context_lazy = pl.lazy_context(skins=S)
-    template = 'panel_rady.html'
+    template = 'panels/panel_rady.html'
     return render(request, template, context_lazy)
 
 
@@ -24,7 +24,17 @@ def staffpanel_h(request):
     # zdefiniuj dodatkowe konteksty tutaj.
     pl = PageLoad(P, L)
     context_lazy = pl.lazy_context(skins=S)
-    template = 'panel_akademika.html'
+    template = 'panels/panel_akademika.html'
+    return render(request, template, context_lazy)
+
+
+# Panel Tłumaczeniowy
+@translators_only(login_url='logger')
+def translatorpanel(request):
+    # zdefiniuj dodatkowe konteksty tutaj.
+    pl = PageLoad(P, L)
+    context_lazy = pl.lazy_context(skins=S)
+    template = 'panels/panel_tlumacza.html'
     return render(request, template, context_lazy)
 
 
@@ -39,7 +49,7 @@ def userpanel(request):
         # zdefiniuj dodatkowe konteksty tutaj.
         pl = PageLoad(P, L)
         context_lazy = pl.lazy_context(skins=S)
-    template = 'panel_uzytkownika.html'
+    template = 'panels/panel_uzytkownika.html'
     return render(request, template, context_lazy)
 
 
@@ -69,5 +79,5 @@ def showmydata(request):
         # zdefiniuj dodatkowe konteksty tutaj.
         pl = PageLoad(P, L)
         context_lazy = pl.lazy_context(skins=S, context=context)
-        template = 'akademik/panel/user/mydata.html'
+        template = 'panels/user/mydata.html'
         return render(request, template, context_lazy)
